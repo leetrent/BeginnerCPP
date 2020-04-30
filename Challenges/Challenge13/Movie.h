c2d7e56c0e214a734c64512a2859c9723f960b83
LeeTrent <lee.trent.1@gmail.com>
2020-04-30 07:43:50 -0500

    Begins challenge 13, adds Movie.h

---
+++ Challenges/Challenge13/Movie.h
@@ -0,0 +1,35 @@
+#ifndef _MOVIE_H_
+#define _MOVIE_H_
+
+#include <string>
+
+class Movie
+{
+    private:
+        std::string movie_name;
+        std::string mpa_rating;
+        int watched_count;
+
+    public:
+
+    // Contructor (3 args)
+    Movie(std::string movie_name, std::string mpa_rating, int watched_count);
+
+    // Copy constructor
+    Movie(const Movie &source);
+
+    // Destructor
+    ~Movie();   
+
+    // Getters
+    std::string get_movie_name() const;
+    std::string get_mpa_rating() const;
+    int get_watched_count() const;
+
+    // Setters
+    void set_movie_name(std::string movie_name);
+    void set_mpa_rating(std::string mpa_rating);
+    void set_watched_count(int watched_count);
+};
+
+#endif // _MOVIE_H_
