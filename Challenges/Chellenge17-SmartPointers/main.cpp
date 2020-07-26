dda732e1c89fe21f716a12e9fd64d0023c52303a
LeeTrent <lee.trent.1@gmail.com>
2020-07-26 09:35:50 -0500

    Starting point for Challenge 16 (Smart Pointers)

---
+++ Challenges/Chellenge17-SmartPointers/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <memory>
+#include <vector>
+using namespace std;
+
+class Test
+{
+    private:
+        int data;
+        void debug(std::string label)
+        {
+            cout << label << " (" << this->data << ")" << endl;
+        }
+    public:
+        Test(): data{0}
+        {
+            debug("Test contructor");
+        }
+        Test(int val): data{val}
+        {
+            debug("Test contructor");
+        }
+        ~Test()
+        {
+            debug("Test destructor");
+        }
+};
+
+///////////////////////////////////////////////////////////
+// Function Prototype:
+///////////////////////////////////////////////////////////
+unique_ptr<vector<shared_ptr<Test>>> make();
+void fill(vector<shared_ptr<Test>> &vec, int num);
+void display(const vector<shared_ptr<Test>> &vec); 
+
+int main()
+{
+    Test t1;
+    Test t2{99};
+    return 0;
+}
