481de4a797c824c8e313f462539141f7cd3300a4
LeeTrent <lee.trent.1@gmail.com>
2020-07-27 09:50:36 -0500

    Completes challenge 17 (smart pointers)

--- Challenges/Chellenge17-SmartPointers/main.cpp
+++ Challenges/Chellenge17-SmartPointers/main.cpp
@@ -24,6 +24,10 @@ class Test
         {
             debug("Test destructor");
         }
+        int get_data()
+        {
+            return this->data;
+        }
 };
 
 ///////////////////////////////////////////////////////////
@@ -33,18 +37,47 @@ unique_ptr<vector<shared_ptr<Test>>> make();
 void fill(vector<shared_ptr<Test>> &vec, int num);
 void display(const vector<shared_ptr<Test>> &vec);
 
+///////////////////////////////////////////////////////////
+// Function main()
+///////////////////////////////////////////////////////////
 int main()
 {
     //Test t1;
     //Test t2{99};
 
     unique_ptr<vector<shared_ptr<Test>>> vec_ptr = make();
+    cout << "How many data points do you want to enter: ";
+    int num;
+    cin >> num;
+    fill(*vec_ptr, num);
+    display(*vec_ptr);
+
     return 0;
+
 }
 
+///////////////////////////////////////////////////////////
+// Function Implementations:
+///////////////////////////////////////////////////////////
 unique_ptr<vector<shared_ptr<Test>>> make()
 {
     //vector<shared_ptr<Test>> vec = make_unique<vector<shared_ptr<Test>>>();
     //return vec;
     return make_unique<vector<shared_ptr<Test>>>();
+}
+
+void fill(vector<shared_ptr<Test>> &vec, int num)
+{
+    for (int ii = 0; ii < num; ii++)
+    {
+        vec.push_back( make_shared<Test>( (ii + 1) * 10) );
+    }
+}
+
+void display(const vector<shared_ptr<Test>> &vec)
+{
+    for ( const shared_ptr<Test> test : vec)
+    {
+        cout << test->get_data() << endl;
+    }
 }
