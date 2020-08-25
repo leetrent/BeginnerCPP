c53dde8dc21a55327c77bef77deea95bedec6387
LeeTrent <lee.trent.1@gmail.com>
2020-08-24 19:15:57 -0500

    Adds exercise 'reading file with 3 words'

---
+++ Exercises/ReadingFIle/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+
+int read_file(std::string);
+
+int main()
+{
+    std::cout << std::endl;
+    return read_file("data.txt");
+    std::cout << std::endl;
+}
+
+int read_file(std::string file_name)
+{
+    std::ifstream in_file {file_name};
+    if ( !in_file.is_open() )
+    {
+        std::cerr << "Cound not open file with this name: '" << file_name << "'." << std::endl;
+        return 1;
+    }
+
+    std::string word{};
+    while (in_file >> word)
+    {
+        std::cout << word << std::endl;
+    }
+
+    return 0;
+}
